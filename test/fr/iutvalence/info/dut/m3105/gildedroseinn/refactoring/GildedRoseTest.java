b222c83d7603028986e44ca1c24022249cca6594
klimczam <mathis.klimczak@iut-valence.fr>
2015-11-03 15:25:18 +0100

    First test Create test method decreaseQualityAndSellInTest()

--- test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
+++ test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
@@ -6,10 +6,19 @@ import org.junit.Test;
 
 public class GildedRoseTest
 {
+	
+	private final static int QUALITY = 50;
+	private final static int SELLIN = 30;
+	
 	@Test
-	public void aSimpleBeginningTest()
+	public void decreaseQualityAndSellInEveryDayTest()
 	{
-		assertEquals(true,false);
+		Item item = new Item("Aged Brie", SELLIN, QUALITY);
+		GildedRose.updateItem(item);
+		assertEquals(item.getQuality(), QUALITY-1);
+		assertEquals(item.getSellIn(), SELLIN-1);
 	}
+	
+	
 
 }
